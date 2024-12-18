8baec27ada8c1ccd05d90986b845ddb3939f9863
limbo <935525020@qq.com>
2024-12-18 19:21:53 +0800

    配置跨域

--- zzy-commons/zzy-common-security/src/main/java/com/yuyou/zizaiyou/config/WebConfig.java
+++ zzy-commons/zzy-common-security/src/main/java/com/yuyou/zizaiyou/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.yuyou.zizaiyou.config;
 import com.yuyou.zizaiyou.jwt.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
@@ -13,6 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Autowired
 	LoginInterceptor loginInterceptor;
+
+	/**
+	 * 注册登录授权拦截器，拦截无有效token的请求
+	 * @param registry
+	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 注册拦截器并设置拦截路径
@@ -20,4 +26,21 @@ public class WebConfig implements WebMvcConfigurer {
 				.addPathPatterns("/**") // 设置拦截的路径
 				.excludePathPatterns("/users/register,/users/login"); // 排除不需要拦截的路径
 	}
+
+
+	/**
+	 * 启用全局 CORS,允许跨域请求
+	 * @param registry
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// 匹配所有路径
+		registry.addMapping("/**") // 匹配所有路径
+				.allowedOrigins("*") // 允许的来源
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+				.allowedHeaders("Content-Type", "Authorization","token") // 允许的请求头
+				.allowCredentials(true) // 是否允许携带凭证
+				.maxAge(3600); // 预检请求的缓存时间
+	}
+
 }
