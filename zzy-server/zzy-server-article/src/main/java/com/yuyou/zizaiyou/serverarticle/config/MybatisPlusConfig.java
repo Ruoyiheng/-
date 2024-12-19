52dd475190c7b80b4c879b98c0fdb31a181ec8f5
yuci <yuci6137@163.com>
2024-12-19 11:07:34 +0800

    配置分页插件

---
+++ zzy-server/zzy-server-article/src/main/java/com/yuyou/zizaiyou/serverarticle/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.yuyou.zizaiyou.serverarticle.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@MapperScan("com.yuyou.zizaiyou.serverarticle.mapper")
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
