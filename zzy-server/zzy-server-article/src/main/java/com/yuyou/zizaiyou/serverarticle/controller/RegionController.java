f41a17c9eb27f794507cdbcd0d6c7fc3c059a0e0
yuci <yuci6137@163.com>
2024-12-22 17:01:27 +0800

    前后端联调

--- zzy-server/zzy-server-article/src/main/java/com/yuyou/zizaiyou/serverarticle/controller/RegionController.java
+++ zzy-server/zzy-server-article/src/main/java/com/yuyou/zizaiyou/serverarticle/controller/RegionController.java
@@ -25,24 +25,52 @@ public class RegionController {
         this.regionService = regionService;
     }
 
+    /**
+     * @param id
+     * @return
+     * 通过id进行查询
+     */
     @GetMapping("/detail")
     public BaseResponse<Region> getById(Long id) {
         Region region = regionService.getById(id);
         return ResultUtils.success(region);
     }
 
-    @GetMapping("/page")
+    /**
+     * @param queryObject
+     * @return
+     * 分页查询区域信息
+     */
+    @GetMapping("/query")
     public BaseResponse<Page<Region>> pageList(QueryObject queryObject) {
         return ResultUtils.success(regionService.queryPage(queryObject));
     }
-    @PostMapping("/add")
+
+    /**
+     * @param region
+     * @return
+     * 新增区域
+     */
+    @PostMapping("/save")
     public BaseResponse<Boolean> addRegion(Region region){
         return ResultUtils.success(regionService.save(region));
     }
+
+    /**
+     * @param id
+     * @return
+     * 通过id对地区进行删除
+     */
     @PostMapping("/delete/{id}")
     public BaseResponse<Boolean> deleteRegion(Long id){
         return ResultUtils.success(regionService.removeById(id));
     }
+
+    /**
+     * @param region
+     * @return
+     * 修改区域信息
+     */
     @PostMapping("/update")
     public BaseResponse<Boolean> updateRegion(Region region){
         return ResultUtils.success(regionService.updateById(region));
